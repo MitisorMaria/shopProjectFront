6a87f924a2fa778f931ac38069c5dcd6c67b00aa
maria <maria.mitisor@gmail.com>
2021-01-27 09:18:29 +0200

    homepage

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path:  '', pathMatch:  "full", redirectTo:  "home" },
+  { path: 'home', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent},
+  { path: 'contact', component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
