18f0a7512c5f9b1c98134f54c410a9416ad7973a
maria <maria.mitisor@gmail.com>
2021-01-28 18:13:18 +0200

    necklaces catalog component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BraceletsComponent } from './bracelets/bracelets.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ContactComponent } from './contact/contact.component';
 import { FavouritesComponent } from './favourites/favourites.component';
 import { HomeComponent } from './home/home.component';
+import { NecklacesComponent } from './necklaces/necklaces.component';
+import { RingsComponent } from './rings/rings.component';
 import { SignupComponent } from './signup/signup.component';
 
 
@@ -15,7 +18,10 @@ const routes: Routes = [
   { path: 'contact', component: ContactComponent},
   { path: 'signup', component: SignupComponent },
   { path: 'checkout', component: CheckoutComponent },
-  { path: 'favourites', component: FavouritesComponent}
+  { path: 'favourites', component: FavouritesComponent},
+  { path: 'necklaces', component: NecklacesComponent },
+  { path: 'bracelets', component: BraceletsComponent },
+  { path: 'rings', component: RingsComponent }
 ];
 
 @NgModule({
